702b68caef02e6269e34bbdb1ebae9920388d660
Eason Tsui <hibfnv@hotmail.com>
2020-08-27 22:43:51 +0800

    Add files via upload

---
+++ rocket_sets.py
@@ -0,0 +1,7 @@
+"""这里存放主屏幕中关于屏幕显示的所有相关设置"""
+class RocketSets():
+    def __init__(self):
+        self.screen_width = 1280
+        self.screen_height = 800
+        self.bg_color = (87, 250, 255)
+        self.speedrate = 0.5
